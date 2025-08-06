1387a0ac28bed5a97e26b105283f6e63d6c09ebb
Dharneesh N <dharneesh333@gmail.com>
2025-08-06 22:43:22 +0530

    feature-Added dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
-    <>
+    <div className="justify-center">
       <h1>Hello, Next.js!</h1>
       <Button>Click me</Button>
-    </>
+      <div className="absolute top-1 right-4">
+        <ModeToggle />
+      </div>
+    </div>
   );
 }
